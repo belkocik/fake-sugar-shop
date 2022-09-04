c519622f8199a7588a5cfbe64fe42ca295693c66
bubuq3 <62507500+bubuq3@users.noreply.github.com>
2022-09-04 18:43:27 +0100

    feat: empty card caution in cart page

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -27,6 +27,8 @@ import {
   AnimateSharedLayout,
   LayoutGroup,
 } from 'framer-motion';
+import { useState, useEffect } from 'react';
+import NextLink from 'next/link';
 
 const container = {
   hidden: { opacity: 0 },
@@ -47,32 +49,50 @@ const MotionBox = motion<BoxProps>(Box);
 const Cart = () => {
   const selectProducts = useSelector(selectAllDataFromStore);
   // console.log(selectIdOfTheProduct);
+  const [loading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setIsLoading(false);
+    // console.log('CART.LENGTH is ', cart.length);
+  }, []);
+
+  console.log('length of', selectProducts.length);
 
   return (
-    <PageLayout title='Koszyk' description='Fake Sugar - koszyk'>
-      <VStack>
-        <AnimatePresence>
-          {selectProducts.map((item) => {
-            return (
-              // <AnimatePresence>
-              //   <MotionBox variants={container} initial='hidden' animate='show'>
-              //     <motion.div variants={children} key={item.slug}>
-              //       <CartComponent item={item} />
-              //     </motion.div>
-              //   </MotionBox>
-              // </AnimatePresence>
-              <motion.div
-                key={item.slug}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0, x: -200 }}
+    <>
+      <PageLayout title='Koszyk' description='Fake Sugar - koszyk'>
+        <VStack>
+          <AnimatePresence>
+            {selectProducts.map((item) => {
+              return (
+                <motion.div
+                  key={item.slug}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0, x: -200 }}
+                >
+                  <CartComponent item={item} />
+                </motion.div>
+              );
+            })}
+
+            {selectProducts.length < 1 && (
+              <MotionBox
+                initial={{ opacity: 0, y: -400 }}
+                animate={{ opacity: 1, y: 0, transition: { duration: 2 } }}
               >
-                <CartComponent item={item} />
-              </motion.div>
-            );
-          })}
-        </AnimatePresence>
-      </VStack>
-    </PageLayout>
+                <Flex direction='column' align='center' justify='center'>
+                  <Heading as='h3' fontSize='3xl'>
+                    Twój koszyk jest pusty.
+                  </Heading>
+                  <NextLink href='/' passHref>
+                    <Button colorScheme='gray'>Przeglądaj produkty</Button>
+                  </NextLink>
+                </Flex>
+              </MotionBox>
+            )}
+          </AnimatePresence>
+        </VStack>
+      </PageLayout>
+    </>
   );
 };
 

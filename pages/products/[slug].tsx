aa14784d27c30f16d06e96d5a13394067bdcd824
bubuq3 <62507500+bubuq3@users.noreply.github.com>
2022-09-09 20:04:03 +0100

    feat: changed rich text editor a little bit in product slugs

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -2,7 +2,6 @@ import {
   Box,
   Heading,
   Flex,
-  Image,
   Text,
   Icon,
   Tooltip,
@@ -10,6 +9,9 @@ import {
   Button,
   Badge,
   HStack,
+  Stack,
+  Divider,
+  chakra,
 } from '@chakra-ui/react';
 import { GetServerSideProps } from 'next';
 import { gql } from 'graphql-request';
@@ -21,14 +23,25 @@ import hygraph from '@/utils/graphqlRequestClient';
 import { FiShoppingBag } from 'react-icons/fi';
 import { useDispatch } from 'react-redux';
 import NextLink from 'next/link';
-
+import NextImage from 'next/image';
 import { addToCart } from 'src/redux/slices/cartSlice';
 import useGetItemDetails from '@/utils/useGetItemDetails';
 
+const Image = chakra(NextImage, {
+  baseStyle: { maxH: 120, maxW: 120 },
+  shouldForwardProp: (prop) =>
+    [
+      'width',
+      'height',
+      'src',
+      'alt',
+      'quality',
+      'placeholder',
+      'blurDataURL',
+    ].includes(prop),
+});
+
 const ProductSlugPage = ({ product }) => {
-  // console.log(product.sugars[0]);
-  // const { title, price, description, brand, coverImage } = product.sugars[0];
-  console.log(product);
   const {
     id,
     title,
@@ -55,67 +68,91 @@ const ProductSlugPage = ({ product }) => {
         rounded='lg'
         w='100%'
         textAlign='left'
+        align='center'
       >
         <Box>
           <Image
             src={coverImage.url}
-            width={{ base: '700px', lg: 'full' }}
-            height='500px'
+            width={700}
+            height={500}
+            quality={10}
             rounded='lg'
+            placeholder='blur'
+            blurDataURL={coverImage.url}
             alt={title}
-            fallbackSrc='https://via.placeholder.com/700x500'
             objectFit='fill'
           />
         </Box>
 
         <Box p={6}>
-          <Heading
-            as='h1'
-            fontSize={'4xl'}
-            fontFamily={'body'}
-            fontWeight={500}
+          <Stack
+            align={{ base: 'left', lg: 'center' }}
+            justify='center'
+            bg='gray.100'
+            p={6}
+            rounded='lg'
+            mb={4}
+            direction={{ base: 'column', md: 'row' }}
           >
-            {title}
-          </Heading>
-          <Box
-            textAlign='center'
-            justifyContent='center'
-            width='300px'
-            mb={2}
-            mt={2}
-          >
-            <HStack align='center'>
-              {isOnDiscount && isNewProduct ? (
-                <Flex gap={1}>
-                  <Badge colorScheme='purple'>Nowy</Badge>
-                  <Badge colorScheme='red'>Promocja -{discountValue}%</Badge>
-                </Flex>
-              ) : isOnDiscount ? (
-                <Badge colorScheme='red'>Promocja -{discountValue}%</Badge>
-              ) : isNewProduct ? (
-                <Badge colorScheme='purple'>Nowy</Badge>
-              ) : null}
-            </HStack>
-          </Box>
-          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-            {brand}
-          </Text>
-          <Text color='blue.400'>Dostepność: {stock}</Text>
+            <Box>
+              <Heading
+                as='h1'
+                fontSize={'4xl'}
+                fontFamily={'body'}
+                fontWeight={500}
+              >
+                {title}
+              </Heading>
+              <Box
+                textAlign='center'
+                justifyContent='center'
+                width='300px'
+                mb={2}
+                mt={2}
+              >
+                <HStack align='center'>
+                  {isOnDiscount && isNewProduct ? (
+                    <Flex gap={1}>
+                      <Badge colorScheme='purple'>Nowy</Badge>
+                      <Badge colorScheme='red'>
+                        Promocja -{discountValue}%
+                      </Badge>
+                    </Flex>
+                  ) : isOnDiscount ? (
+                    <Badge colorScheme='red'>Promocja -{discountValue}%</Badge>
+                  ) : isNewProduct ? (
+                    <Badge colorScheme='purple'>Nowy</Badge>
+                  ) : null}
+                </HStack>
+              </Box>
+            </Box>
+            <Stack fontWeight={700} p={6}>
+              <Text
+                color={'gray.500'}
+                fontSize={'sm'}
+                textTransform={'uppercase'}
+              >
+                {brand}
+              </Text>
+              <Text color='blue.400'>Dostępność: {stock}</Text>
+            </Stack>
+          </Stack>
 
           <RichText
             content={description.raw}
             renderers={{
-              h1: ({ children }) => <h1 className='text-normal'>{children}</h1>,
-              h2: ({ children }) => <h2 className='text-normal'>{children}</h2>,
-              h3: ({ children }) => <h3 className='text-normal'>{children}</h3>,
-              h4: ({ children }) => <h4 className='text-normal'>{children}</h4>,
-              p: ({ children }) => <p className='text-normal'>{children}</p>,
-              bold: ({ children }) => (
-                <strong className='text-bold'>{children}</strong>
+              h1: ({ children }) => (
+                <Heading as='h1' color='teal'>
+                  {children}
+                </Heading>
               ),
-              italic: ({ children }) => (
-                <span className='text-italic'>{children}</span>
-              ),
+              h2: ({ children }) => <Heading as='h2'>{children}</Heading>,
+              h3: ({ children }) => <Heading as='h3'>{children}</Heading>,
+              h4: ({ children }) => <Heading as='h4'>{children}</Heading>,
+              p: ({ children }) => <Text>{children}</Text>,
+              bold: ({ children }) => <Text as='b'>{children}</Text>,
+              italic: ({ children }) => <Text as='i'>{children}</Text>,
+              blockquote: ({ children }) => <Text as='u'>{children}</Text>,
             }}
           />
         </Box>

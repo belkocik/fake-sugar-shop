c37eb2ed49eefb80351249c38e6f07e3d1d90eff
bubuq3 <62507500+bubuq3@users.noreply.github.com>
2022-09-15 18:20:03 +0100

    feat: added footer animation

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,75 +1,84 @@
-import { Box, VStack, Link, useColorModeValue } from '@chakra-ui/react';
-import PageLayout from '../page-layout';
+import { Box, VStack, Link, BoxProps } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+
+const MotionBox = motion<BoxProps>(Box);
 
 const Footer = () => {
   return (
-    <VStack
-      align='center'
-      opacity={{ base: '0.6', lg: '0.7' }}
-      fontSize='sm'
-      as='footer'
-      pt={{ base: '36', md: '6' }}
-      pb={6}
+    <MotionBox
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.8, delay: 1 }}
     >
-      <Box>
-        &copy; {new Date().getFullYear()} sklep internetowy drogi-cukier
-      </Box>
+      <VStack
+        align='center'
+        opacity={{ base: '0.6', lg: '0.7' }}
+        fontSize='sm'
+        as='footer'
+        pt={{ base: '36', md: '6' }}
+        pb={6}
+      >
+        <Box>
+          &copy; {new Date().getFullYear()} sklep internetowy drogi-cukier
+        </Box>
 
-      <Box>
-        Built with{' '}
-        <Box as='span' color='red'>
-          ♥
-        </Box>{' '}
-        and{' '}
-        <Link
-          href='https://nextjs.org/'
-          isExternal
-          display='inline-block'
-          position='relative'
-          _focus={{
-            WebkitTapHighlightColor: 'transparent',
-          }}
-          _after={{
-            content: `""`,
-            position: 'absolute',
-            width: '100%',
-            transform: 'scaleX(0)',
-            height: '2px',
-            bottom: 0,
-            left: 0,
-            backgroundColor: '#0087ca',
-            transformOrigin: 'bottom right',
-            transition: 'transform 0.25s ease-out',
-          }}
-          _hover={{
-            _after: {
-              transform: 'scaleX(1)',
-              transformOrigin: 'bottom left',
-            },
-          }}
-          color={useColorModeValue('blue.500', 'blue.300')}
-        >
-          Next.js
-        </Link>{' '}
-        by{' '}
-        <Link
-          href='https://www.marcinniedbalec.xyz/'
-          isExternal
-          _focus={{
-            WebkitTapHighlightColor: 'transparent',
-          }}
-          _hover={{
-            textDecor: 'none',
-            color: 'cyan.400',
-          }}
-          color={useColorModeValue('teal.900', 'teal.300')}
-          textDecoration='none'
-          transition='0.25s ease-out'
-        >
-          Marcin Niedbalec
-        </Link>
-      </Box>
-    </VStack>
+        <Box>
+          Built with{' '}
+          <Box as='span' color='red'>
+            ♥
+          </Box>{' '}
+          and{' '}
+          <Link
+            href='https://nextjs.org/'
+            isExternal
+            display='inline-block'
+            position='relative'
+            _focus={{
+              WebkitTapHighlightColor: 'transparent',
+            }}
+            _after={{
+              content: `""`,
+              position: 'absolute',
+              width: '100%',
+              transform: 'scaleX(0)',
+              height: '2px',
+              bottom: 0,
+              left: 0,
+              backgroundColor: '#0087ca',
+              transformOrigin: 'bottom right',
+              transition: 'transform 0.25s ease-out',
+            }}
+            _hover={{
+              _after: {
+                transform: 'scaleX(1)',
+                transformOrigin: 'bottom left',
+              },
+            }}
+            color='blue.500'
+          >
+            Next.js
+          </Link>{' '}
+          by{' '}
+          <Link
+            href='https://www.marcinniedbalec.xyz/'
+            isExternal
+            _focus={{
+              WebkitTapHighlightColor: 'transparent',
+            }}
+            _hover={{
+              textDecor: 'none',
+              color: 'cyan.400',
+            }}
+            color='teal.900'
+            textDecoration='none'
+            transition='0.25s ease-out'
+          >
+            Marcin Niedbalec
+          </Link>
+        </Box>
+      </VStack>
+    </MotionBox>
   );
 };
 
